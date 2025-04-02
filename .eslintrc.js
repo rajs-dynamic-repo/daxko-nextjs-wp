1d35ca15b44792ee666b84acdc98d195f01e970e
Raj Shekhar Shukla <119837403+rajs-dynamic-repo@users.noreply.github.com>
2025-04-02 13:37:23 +0530

    adding export to eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
-module.exports = {
+exports = {
   extends: "next/core-web-vitals",
   rules: {
     "@next/next/no-img-element": "off", // Disable <img> warnings
     "@typescript-eslint/no-explicit-any": "off", // Allow 'any' type in TypeScript
   },
 };
+
